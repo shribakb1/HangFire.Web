c2abb4aeb5ac6f2dbe1816173431551970933249
Shribak Bohdan <shribakb@gmail.com>
2025-10-31 17:50:57 +0200

    feat: add scheduled job - download files

--- HangFire.Web/Program.cs
+++ HangFire.Web/Program.cs
@@ -1,5 +1,6 @@
 using Hangfire;
 using HangFire.Web.Jobs;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,5 +31,40 @@ app.MapGet("/pull", (IBackgroundJobClient client) =>
 
     client.Enqueue<WebPuller>(x => x.GetRssItemUrlAsync(url, tempPath));
 });
+
+app.MapGet("/sync", (IBackgroundJobClient client) =>
+{
+    var directory = $"d:\\rss";
+    var fileName = "consultwithgriff_rss_urls.json";
+
+    var path = Path.Combine(directory, fileName);
+    var json = File.ReadAllText(path);
+    var rssItemUrls = JsonSerializer.Deserialize<List<string>>(json);
+
+    var outputPath = Path.Combine(directory, "output");
+    if (!Directory.Exists(outputPath)) Directory.CreateDirectory(outputPath);
+
+    if (rssItemUrls == null || rssItemUrls.Count == 0) return;
+
+    var delayInSeconds = 5;
+
+    foreach (var url in rssItemUrls)
+    {
+        var u = new Uri(url);
+        var stub = u.Segments.Last();
+
+        if (stub.EndsWith("/")) stub = stub.Substring(0, stub.Length - 1);
+        stub += ".html";
+
+        var filePath = Path.Combine(outputPath, stub);
+
+        client.Schedule<WebPuller>(p => p.DownloadFileFromUrl(url, filePath),
+            delay: TimeSpan.FromSeconds(delayInSeconds));
+
+        delayInSeconds += 5;
+    }
+
+});
+
 
 app.Run();
